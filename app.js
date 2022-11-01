1689ff7591380249f12b069d0ec6ba81f396db80
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-01 11:19:23 -0700

    Added button functionality to buttons 2 and 3

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 /* Get DOM Elements */
 const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
 const img1 = document.querySelector('#img-1');
 const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
@@ -29,6 +31,42 @@ button1.addEventListener('click', () => {
     } else if (pearlLocation === 2) {
         img2.classList.add('reveal');
     } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button2.addEventListener('click', () => {
+    total++;
+    resetPearls();
+    // generate a random location for the pearl (number between 1 and 3)
+    const pearlLocation = Math.ceil(Math.random() * 3);
+    console.log('pearlLocation', pearlLocation);
+    // add reveal class to img that lines up with random location (if..else)
+    if (pearlLocation === 1) {
+        img1.classList.add('reveal');
+    } else if (pearlLocation === 2) {
+        wins++;
+        img2.classList.add('reveal');
+    } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button3.addEventListener('click', () => {
+    total++;
+    resetPearls();
+    // generate a random location for the pearl (number between 1 and 3)
+    const pearlLocation = Math.ceil(Math.random() * 3);
+    console.log('pearlLocation', pearlLocation);
+    // add reveal class to img that lines up with random location (if..else)
+    if (pearlLocation === 1) {
+        img1.classList.add('reveal');
+    } else if (pearlLocation === 2) {
+        img2.classList.add('reveal');
+    } else {
+        wins++;
         img3.classList.add('reveal');
     }
     displayResults();
